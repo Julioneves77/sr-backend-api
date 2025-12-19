d7240dd65f67e92308720377c198f68a2bb0c615
Julio Cesar <julionevesgestor@gmail.com>
2025-12-18 21:08:46 -0300

    sr-backend-api: add start script and server

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.status(200).send("sr-backend-api OK");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
